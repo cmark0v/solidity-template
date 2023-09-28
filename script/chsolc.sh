d2974067723396e4f24dfde09e6cc6f14c0cd7af
cmark0v <cmark0v@noreply>
2023-09-28 13:04:17 +0900

    update

--- script/chsolc.sh
+++ script/chsolc.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #change solc in config files for brownie plus all contracts in the contracts folter                                                                                                                                                         
-export FF=`ls contracts/*.sol|head -n 1`
+export CFOLDER=src
+export FF=`ls $CFOLDER/*.sol|head -n 1`
 export SOLC_VERSION=`grep -ow '\\^[0-9]\\.[0-9]\\.[0-9]\{1,2\}' $FF|cut -c 2-`
+export CFGFILES=foundry.toml
+export CONTRACTS=`find .|grep \\.sol$`
+echo $CONTRACTS 
+echo $CFGFILES
 if [ $1 ]
 then
 echo "changing solc $SOLC_VERSION changing to $1"
@@ -12,21 +17,19 @@ fi
 export CHK=`echo $1|grep -ow '[0-9]\\.[0-9]\\.[0-9]\{1,2\}'`
 if [ $CHK ] 
 then
-    sed -i "s/$SOLC_VERSION/$CHK/" foundry.toml
-    echo "change brownie"
-    sed -i "s/pragma solidity \\^$SOLC_VERSION/pragma solidity \\^$CHK/" contracts/*.sol
+    sed -i "s/$SOLC_VERSION/$CHK/" $CFGFILES
+    echo "change cfg files"
+    sed -i "s/pragma solidity \\^[0-9]\\.[0-9]\\.[0-9]\{1,2\}/pragma solidity \\^$CHK/" $CONTRACTS
     echo "change contracts"
-    sed -i "s/pragma solidity \\^$SOLC_VERSION/pragma solidity \\^$CHK/" contracts/*/*.sol
-    echo "change libs"
-    export CHKLIB=`cat contracts/*/*.sol contracts/*.sol brownie-config.yaml|grep $CHK|wc -l`
-    export _COUNT=`ls contracts/*/*.sol contracts/*.sol brownie-config.yaml|wc -l`
+    export CHKLIB=`cat $CONTRACTS $CFGFILES|grep $CHK|wc -l`
+    export _COUNT=`ls $CONTRACTS $CFGFILES|wc -l`
     if [ $CHKLIB.eq.$_COUNT ]
     then
-        echo "looks g2g, all files updated"
+        echo "looks g2g, all files updated $CHKLIB"
     else
         echo "$CHKLIB out of $_COUNT files changes"
     fi
 else
     echo "$1 doesnt look right for a version number to me"
 fi
-
+export SOLC_VERSION=$1
